1eef58857ba08119567409c00d8ddf44eb6ac3a9
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-05-08 10:26:51 -0500

    Init EA projects

---
+++ src/test/java/edu/mum/ea/EaApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.mum.ea;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
