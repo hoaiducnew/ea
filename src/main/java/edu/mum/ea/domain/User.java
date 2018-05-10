514e5f0e611e20b94160847a46243ba21ac2dee6
Hoai Duc Nguyen <hoaiducnew@gmail.com>
2018-05-10 18:59:07 -0500

    add Address to User

--- src/main/java/edu/mum/ea/domain/User.java
+++ src/main/java/edu/mum/ea/domain/User.java
@@ -1,6 +1,7 @@
 package edu.mum.ea.domain;
 
 import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -23,6 +24,9 @@ public class User {
 	private String phone;
 
 	private boolean enabled;
+
+	@Embedded
+	private Address address;
 
 	public long getId() {
 		return id;
@@ -70,5 +74,13 @@ public class User {
 
 	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
 	}
 }
